fbf8e07d704aec30b1c0026846c91c2fb26ad774
juancamargo01 <74567426+juancamargo01@users.noreply.github.com>
2021-01-07 21:13:59 -0300

    correção  do erro  do java script
    erro de logica na linhas 64 65 66

--- _js/game.js
+++ _js/game.js
@@ -58,32 +58,32 @@ function inicializarEspacos()
 
 }
 
-function verificarVencedor(){
+async function verificarVencedor(){
 
     var a1 =document.getElementById("a1").getAttribute("jogada");
-    var a2 =document.getElementById("a1").getAttribute("jogada");
-    var a3 =document.getElementById("a1").getAttribute("jogada");
+    var a2 =document.getElementById("a2").getAttribute("jogada");
+    var a3 =document.getElementById("a3").getAttribute("jogada");
 
-    var b1 =document.getElementById("a1").getAttribute("jogada");
-    var b2 =document.getElementById("a1").getAttribute("jogada");
-    var b3 =document.getElementById("a1").getAttribute("jogada");
+    var b1 =document.getElementById("b1").getAttribute("jogada");
+    var b2 =document.getElementById("b2").getAttribute("jogada");
+    var b3 =document.getElementById("b3").getAttribute("jogada");
 
-    var c1 =document.getElementById("a1").getAttribute("jogada");
-    var c2 =document.getElementById("a1").getAttribute("jogada");
-    var c3 =document.getElementById("a1").getAttribute("jogada");
+    var c1 =document.getElementById("c1").getAttribute("jogada");
+    var c2 =document.getElementById("c2").getAttribute("jogada");
+    var c3 =document.getElementById("c3").getAttribute("jogada");
 
 
     var vencedor ="";
-    if(( (a1 == b1 && a1 == c1 ) || (a1 == a2 && a1 == a3& a1 ) || (a1 == b2 && a1 == c3 ) ) && a1 != ""  )
+    if(( (a1 == b1 && a1 == c1 ) || (a1 == a2 && a1 == a3) || (a1 == b2 && a1 == c3 ) ) && a1 != "" )
     {
         vencedor = a1;
     }
-    else if(((b2 == b1 && b2 == b3)||(b2 == a2 && b2 == c2)||(b2 == a3 && b2 == c1 )) && a1 != "" )
+    else if(( (b2 == b1 && b2 == b3) || (b2 == a2 && b2 == c2) || ( b2 == a3 && b2 == c1 ) ) && b2 != "" )
     {
         vencedor = b2;
 
     }
-    else if(((c3 == c2 && c3 == c1)||(c3 == a3 && c3 == b3)) && a1 != "")
+    else if(( (c3 == c2 && c3 == c1) || ( c3 == a3 && c3 == b3) ) &&  c3 != "")
     {
         vencedor = c3;
     }
@@ -91,11 +91,11 @@ function verificarVencedor(){
     if (vencedor != "")
     {
         fimDeJogo = true;
-        //await sleep();
+        await sleep(50);
         alert("o ganhador foi o : '" + vencedor + "'");
     }
 }
-/*function sleep(ms)
+function sleep(ms)
 {
     return new Promise(resolve => setTimeout(resolve,ms));
-}*/
+}
